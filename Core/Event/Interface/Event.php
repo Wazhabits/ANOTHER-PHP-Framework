a08510ed55ba9e4cb7e8531de3a48cbb4536efaf
Anatole <anatole.piveteau@imie.fr>
2019-12-16 22:24:44 +0100

    Init Event

---
+++ Core/Event/Interface/Event.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Core\Event;
+
+interface Event
+{
+    /**
+     * This function add listener to an event
+     * @param $eventName = "Namespace/class.method"
+     * @param $classnameAndMethod : "My\Class->myMethod()"
+     * @return mixed
+     */
+    static function add($eventName, $classnameAndMethod);
+
+    /**
+     * This function execute all listener associated to an event
+     * @param $eventName
+     * @return mixed
+     */
+    static function exec($eventName);
+}

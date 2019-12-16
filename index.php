22c921751d39f55f195542bd557be4384f23d8e0
Anatole <anatole.piveteau@imie.fr>
2019-12-16 11:37:23 +0100

    Add documentaton & comment

--- index.php
+++ index.php
@@ -1,11 +1,21 @@
 <?php
 
+/**
+ * This file is part of the Framework project
+ * Copyright 2019 - Core team
+ */
+
 include_once __DIR__ . "/Core/Loader/Loader.php";
 
 use Core\Loader;
 
-
+/**
+ * Including classes sorting by constraints
+ */
 Loader::explore(__DIR__, "Interface");
 Loader::explore(__DIR__, "", "Interface");
 
+/**
+ * Initialize kernel
+ */
 \Core\Kernel::initialize();

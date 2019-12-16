19842540113689059a538a57112eb06d9dcebb18
Anatole <anatole.piveteau@imie.fr>
2019-12-16 11:16:07 +0100

    Working on loader

--- Core/Loader/Loader.php
+++ Core/Loader/Loader.php
@@ -8,15 +8,16 @@ use \Core\Loader\LoaderBase;
 
 class Loader implements LoaderBase
 {
-    static function explore($path, $needle = "", $constraint = "")
+    static function explore($path, $needle = "", $constraint = "", $depth = 0)
     {
         $scan = glob($path . DIRECTORY_SEPARATOR . "*");
         foreach ($scan as $path) {
             if (is_dir($path)) {
-                self::explore($path, $needle, $constraint);
+                self::explore($path, $needle, $constraint, $depth + 1);
             } else {
                 if (strpos($path, ".php") !== false) {
-                    if (($needle === "" || strpos($path, $needle) !== false) && ($constraint === "" || strpos($path, $constraint) === false)) {
+                    if (($needle === "" || strpos($path, $needle) !== false) && ($constraint === "" || strpos($path, $constraint) === false) && $depth !== 0) {
+                        var_dump($path);
                         require_once $path;
                     }
                 }

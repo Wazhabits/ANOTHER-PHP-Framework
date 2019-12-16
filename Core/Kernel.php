c7d56c8c6bc4946a6a5093d61261509a325fbfda
Anatole <anatole.piveteau@imie.fr>
2019-12-16 13:41:58 +0100

    Fixup kernel

--- Core/Kernel.php
+++ Core/Kernel.php
@@ -2,14 +2,20 @@
 
 namespace Core;
 
-
 class Kernel
 {
+    /**
+     * @var Env $environment
+     */
     static $environment;
 
     static function initialize() {
         self::$environment = new Env(ROOT_DIRECTORY . ".env");
         Event::create("coucou");
         Logger::log("general", "[KERNEL]::Initialize", Logger::$DEFAULT_LEVEL);
+    }
+
+    static function getEnvironment() {
+        return self::$environment;
     }
 }

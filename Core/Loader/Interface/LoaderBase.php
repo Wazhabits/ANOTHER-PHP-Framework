746e065d5192c88936499b64e5bca965f48aa042
Anatole <anatole.piveteau@imie.fr>
2019-12-16 11:38:50 +0100

    Add comment

--- Core/Loader/Interface/LoaderBase.php
+++ Core/Loader/Interface/LoaderBase.php
@@ -4,5 +4,13 @@ namespace Core\Loader;
 
 interface LoaderBase
 {
-
+    /**
+     * This function include all PHP files found in given directory (recursive)
+     * @param $path
+     * @param string $needle
+     * @param string $constraint
+     * @param int $depth
+     * @return mixed
+     */
+    static function explore($path, $needle = "", $constraint = "", $depth = 0);
 }

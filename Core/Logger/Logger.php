9492eff2251dc8788781d782f09abd8e67a5e98f
Anatole <anatole.piveteau@imie.fr>
2019-12-16 20:16:44 +0100

    Add comment

--- Core/Logger/Logger.php
+++ Core/Logger/Logger.php
@@ -11,6 +11,13 @@ class Logger implements LoggerBase
     static $DEFAULT_LEVEL = 0;
     static $FOLDERS = ["", "warning", "error"];
 
+    /**
+     * This function write in log file split by specific format
+     * @param string $key is use as sub-folder of Logs folder
+     * @param string $message log content
+     * @param int $status this value is needed for split log like warning/errors
+     * @return bool
+     */
     public static function log($key = "", $message = "", $status = 0)
     {
         if ($key === "" || $message === "")
@@ -19,6 +26,12 @@ class Logger implements LoggerBase
         return true;
     }
 
+    /**
+     * This function build the log file path
+     * @param $key
+     * @param $status
+     * @return string
+     */
     private static function makeLogPath($key, $status) {
         $directory =  PATH_LOG . DIRECTORY_SEPARATOR . $key . DIRECTORY_SEPARATOR
             . self::$FOLDERS[$status] . DIRECTORY_SEPARATOR;

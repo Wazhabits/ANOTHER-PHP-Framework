856c0a0fdb8502d0b2578c94d1ff6c03436316dd
Anatole <anatole.piveteau@imie.fr>
2019-12-30 00:53:54 +0100

    Ajax list

--- Public/main.js
+++ Public/main.js
@@ -1,4 +1,25 @@
+function updateList() {
+    $.ajax({
+        type: "GET",
+        url: "http://91.162.251.47:80/",
+        success: function (data) {
+            var string = "",
+                i = 0,
+                result = Object.values(data);
+            while (i < result.length) {
+                console.log(result[i]);
+                string += "<div class='element' data-src='" + result[i].path + "'><h2>" + result[i].title + "</h2>" +
+                    "<p><strong>" + result[i].title + "</strong><br><strong>" + result[i].author + " - " + result[i].date + "</strong><br>" + result[i].description + "</p></div>";
+                i++;
+            }
+            console.log("HERE '" + string + "'");
+            $(".list").html(string);
+        }
+    });
+}
+
 $(document).ready(function () {
+    updateList();
     $("body").on("click", ".element", function () {
         $("#scene").toggleClass("show");
         $("#scene>.screen").html("<video controls=\"controls\" preload=\"metadata\"><source src='http://91.162.251.47:80/" + $(this).attr("data-src") + "' type='video/mp4'></video>")
@@ -7,5 +28,9 @@ $(document).ready(function () {
         event.preventDefault();
         $($(this).attr("href")).toggleClass("show");
         $("#scene>.screen").html()
+    })
+    $("#refresh").on("click", function (event) {
+        event.preventDefault();
+        updateList();
     })
 });

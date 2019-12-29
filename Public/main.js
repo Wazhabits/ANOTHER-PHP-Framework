542d11791e96bea1b7067273302a03deb0f051cc
Anatole <anatole.piveteau@imie.fr>
2019-12-29 21:13:10 +0100

    Work on streaming site

--- Public/main.js
+++ Public/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     $("body").on("click", ".element", function () {
         $("#scene").toggleClass("show");
-        $("#scene>.screen").html("<video><source src='" + $(this).attr("data-src") + "' type='video/mp4'></video>")
+        $("#scene>.screen").html("<video controls=\"controls\" preload=\"metadata\"><source src='http://91.162.251.47:80/" + $(this).attr("data-src") + "' type='video/mp4'></video>")
 
     }).on("click", ".exit", function (event) {
         event.preventDefault();

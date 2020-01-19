7311158679d83e877dc56ee8948e3ccf7af1efd3
Anatole <anatole.piveteau@imie.fr>
2020-01-19 22:19:23 +0100

    Add datetime to models

--- Core/Database/Model/Model.php
+++ Core/Database/Model/Model.php
@@ -7,11 +7,21 @@ use Core\Database\Model\Model as Base;
 
 class Model implements Base
 {
+    public $sorting;
+    public $createdate;
+    public $updatedade;
+
     public function __construct($element, $sorting)
     {
+        $this->createdate = $this->updatedade = time();
+        $this->sorting = &$sorting;
         foreach ($element as $property => $value) {
             if (!is_numeric($property))
-                $this->{$property} = $value;
+                $this->{$property} = &$value;
         }
+    }
+
+    public function hash(&$value) {
+        $value = password_hash($value, PASSWORD_DEFAULT);
     }
 }

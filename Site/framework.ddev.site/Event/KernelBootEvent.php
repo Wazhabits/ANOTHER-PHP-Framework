d3c5f5a4c8d4db500c03e2a71fab913eb86aaf2e
Anatole <anatole.piveteau@imie.fr>
2020-01-27 11:28:02 +0100

    Fixup

--- Site/framework.ddev.site/Event/KernelBootEvent.php
+++ Site/framework.ddev.site/Event/KernelBootEvent.php
@@ -12,20 +12,18 @@ use Framework\Model\Classes;
 class KernelBootEvent
 {
     /**
-     * @event core/template.postProcess
+     * @event core/kernel.boot
      */
     static function makeDocumentation() {
         $classes = Kernel::getAnnotation()->getDocumentation();
         Logger::log("general", "Loaded classes : " . count($classes));
         foreach ($classes as $class => $property) {
             $exist = Database\Manager::getConnection("mysql")->getQueryBuilder(Classes::class)->select("*")->from(Classes::class)->where([["classname", "=", $class]])->limit(1)->execute();
-            if (isset($exist[0])) {
-                $model = $exist[0];
-            } else {
+            if (!isset($exist[0])) {
                 $model = new Classes(["classname" => $class, "json" => json_encode($property)], 0);
                 Logger::log("general", "New class detected : " . $class);
+                $model->save();
             }
-            $model->save();
         }
     }
 }

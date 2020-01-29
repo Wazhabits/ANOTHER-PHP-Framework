a65eeec5847ff993f0b1a37f7a6ac5ffa0e21c19
Anatole <anatole.piveteau@imie.fr>
2020-01-29 16:07:59 +0100

    Fixup on documentation make & show

--- Site/framework.ddev.site/Modules/Documentation/Controller/DefaultController.php
+++ Site/framework.ddev.site/Modules/Documentation/Controller/DefaultController.php
@@ -4,13 +4,16 @@ namespace Modules\Documentation\Controller;
 
 use Core\Controller;
 use Core\Database\Manager;
+use Core\Logger;
 use Core\Response;
 use Framework\Model\Classes;
 use Framework\Repository\ClassesRepository;
+use Modules\Documentation\Maker;
 
 class DefaultController extends Controller
 {
     /**
+     * Get all documentation (json)
      */
     public function index() {
         $repository = new ClassesRepository();
@@ -18,5 +21,15 @@ class DefaultController extends Controller
         Response::setHeader(["Content-Type" => "application/json"]);
         Response::send();
         echo json_encode($result);
+    }
+
+    /**
+     * Documentation maker
+     */
+    public function make() {
+        Logger::log("documentation", "Builder running", Logger::$DEFAULT_LEVEL);
+        $maker = new Maker();
+        $maker->extract();
+        Response::setHeader(["location" => "/api/documentation"]);
     }
 }

71f633a5bf1641ec753b60e26ed6d5991dc9e62d
Anatole <anatole.piveteau@imie.fr>
2020-01-29 11:26:54 +0100

    Add 2 foreign table of Classes model (Methods & Properties) Add begin work on Repository

---
+++ Site/framework.ddev.site/Model/Method.php
@@ -0,0 +1,33 @@
+<?php
+
+
+namespace Framework\Model;
+
+
+use Core\Database\Model;
+
+class Method extends Model
+{
+    /**
+     * @var string $name
+     * @type varchar
+     * @nullable false
+     * @size 100
+     */
+    public $name;
+
+    /**
+     * @var array $configuration
+     * @type varchar
+     * @size 1024
+     */
+    public $configuration;
+
+    /**
+     * @var
+     * @type int
+     * @size 11
+     * @foreign Framework\Model\Classes
+     */
+    public $class;
+}

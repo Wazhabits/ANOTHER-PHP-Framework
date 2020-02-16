bf8d24add6a94e92cb89b8333a636de97a255b91
Anatole <anatole.piveteau@imie.fr>
2020-02-16 23:27:54 +0100

    Work on comment Refacto on template

--- Modules/Statistic/Event/templatePreProcess.php
+++ Modules/Statistic/Event/templatePreProcess.php
@@ -5,34 +5,59 @@ namespace Modules\Statistic\Event;
 
 
 use Core\Database\Manager;
+use Modules\Statistic\Model\Comment;
 use Modules\Statistic\Model\Pages;
 use Modules\Statistic\Model\Statistic;
 
 class templatePreProcess
 {
     /**
+     * get current page stats
+     * @param $path
+     * @return mixed
+     */
+    static function getPage($path) {
+        return Manager::getConnection("mysql")
+            ->getQueryBuilder(Pages::class)
+            ->select("*")
+            ->from(Pages::class)
+            ->where([["url", "=", str_replace("|", "/", urldecode($path))]])
+            ->execute();
+    }
+
+    static function getVote($id) {
+        $vote = Manager::getConnection("mysql")->getQueryBuilder(Statistic::class)->select("*")->from(Statistic::class)->where([["pageid", "=", $id]])->execute();
+        $like = 0;
+        $dislike = 0;
+        if (is_array($vote)) {
+            foreach ($vote as $v) {
+                if ($v->result == 1) {
+                    $like++;
+                } else {
+                    $dislike++;
+                }
+            }
+        }
+        $temp["like"] = ($like === 0) ? "0 %" : (100 * ($like + $dislike)) / $like . "%";
+        $temp["dislike"] = ($dislike === 0) ? "0 %" : (100 * ($like + $dislike)) / $dislike . " %";
+        return $temp;
+    }
+
+    static function getComment($id) {
+        return Manager::getConnection("mysql")->getQueryBuilder(Comment::class)->select("*")->from(Comment::class)->where([["pageid", "=", $id]])->execute();
+    }
+
+    /**
      * @event core/template.preProcess
      * @param array &$args
      */
     static function getStats(&$args) {
-        $view = Manager::getConnection("mysql")->getQueryBuilder(Pages::class)->select("*")->from(Pages::class)->where([["url", "=", $_SERVER["REQUEST_URI"]]])->execute();
+        $view = self::getPage($_SERVER["REQUEST_URI"]);
         if (isset($view[0])) {
             $view[0]->view++;
+            $args["__PAGE"]["vote"] = self::getVote($view[0]->id);
             $args["__PAGE"]["view"] = $view[0]->view;
-            $vote = Manager::getConnection("mysql")->getQueryBuilder(Statistic::class)->select("*")->from(Statistic::class)->where([["pageid", "=", $view[0]->id]])->execute();
-            $like = 0;
-            $dislike = 0;
-            if (is_array($vote)) {
-                foreach ($vote as $v) {
-                    if ($v->result == 1) {
-                        $like++;
-                    } else {
-                        $dislike++;
-                    }
-                }
-            }
-            $args["__PAGE"]["like"] = $like;
-            $args["__PAGE"]["dislike"] = $dislike;
+            $args["__COMMENT"] = self::getComment($view[0]->id);
             $view[0]->save();
         } else {
             $view = new Pages(["url" => $_SERVER["REQUEST_URI"], "view" => 0]);
